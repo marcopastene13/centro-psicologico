22bbe39284628a0f29617e332bc9ab249041e820
Marco Pastene <mpastene1996@outlook.com>
2025-07-06 06:31:22 +0000

    pruebas colores

--- centro-psicologico/src/pages/Home.jsx
+++ centro-psicologico/src/pages/Home.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Equipo from "../components/Equipo";
-import Novedades from "../components/Novedades"; // Importamos el componente novedades
+import Novedades from "../components/Novedades";
 import "./Home.css";
 
 const Home = () => {
   return (
     <>
-      <div className="home-jumbotron d-flex align-items-center">
+      <div
+        className="home-jumbotron d-flex align-items-center"
+        style={{
+          backgroundImage: "url('/logo.centro.jpg')", // Ruta de tu imagen
+          backgroundSize: "contain",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          minHeight: "90vh",
+          position: "relative",
+          color: "white",
+        }}
+      >
+        <div className="home-overlay"></div>
         <Container>
           <Row>
             <Col md={8}>
@@ -28,20 +40,7 @@ const Home = () => {
       <div
         style={{
           textAlign: "center",
-          backgroundColor: "#6495ED",
-          padding: "20px",
-          marginTop: "20px",
-        }}
-      >
-        <h2>Novedades en Psicología</h2>
-      </div>
-
-      <Novedades />
-
-      <div
-        style={{
-          textAlign: "center",
-          backgroundColor: "#6495ED",
+          backgroundColor: "#7abfa2",
           padding: "20px",
           marginTop: "20px",
         }}
@@ -52,6 +51,19 @@ const Home = () => {
       <section id="equipo">
         <Equipo />
       </section>
+
+      <div
+        style={{
+          textAlign: "center",
+          backgroundColor: "#7abfa2",
+          padding: "20px",
+          marginTop: "10px",
+        }}
+      >
+        <h2>Novedades en Psicología</h2>
+      </div>
+
+      <Novedades />
     </>
   );
 };
